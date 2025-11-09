1fc1016d2ee6022c846a1319b1c70166f27bf058
mehedihasan770 <mehedihasantrbd@gmail.com>
2025-11-09 10:16:07 +0600

    add Google Sign-In method to AuthProvider

--- src/authContext/AuthProvider.jsx
+++ src/authContext/AuthProvider.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import { useState } from 'react';
 import { AuthContext } from './AuthContext';
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { auth } from '../firebase/Firebase';
+
+const googleProvider = new GoogleAuthProvider()
 
 const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
+
+    const signInWithGoogle = () => {
+        setLoading(true)
+        return signInWithPopup(auth, googleProvider)
+    }
 
     const userInfo = {
-        name: 'Mehedi'
+        user,
+        setUser,
+        loading,
+        setLoading,
+        signInWithGoogle,
     }
 
     return (
